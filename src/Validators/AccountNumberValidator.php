80fad9ea0732e5a114b2f9af63ad6dee9cbc5e32
TangRufus <tangrufus@gmail.com>
2019-10-18 10:39:00 +0100

    Refactor; Validate `IsCorrect`; Clear transient caches when service key updated

--- src/Validators/AccountNumberValidator.php
+++ src/Validators/AccountNumberValidator.php
@@ -4,26 +4,16 @@ declare(strict_types=1);
 
 namespace Itineris\GFLoqateBankVerification\Validators;
 
-use GF_Field;
 use Itineris\GFLoqateBankVerification\API\Result;
 
 class AccountNumberValidator extends AbstractValidator
 {
-    use ValidationMessageConstructTrait;
+    use SetAccountNumberFieldValidationMessageTrait;
+    use NullSortCodeFieldValidationMessageTrait;
+    use ValidationMessageTrait;
 
     protected function shouldIntercept(Result $result): bool
     {
         return $result->isInvalidAccountNumber();
-    }
-
-    protected function markSortCodeFieldAsFailed(GF_Field $field): void
-    {
-        // Do nothing.
-    }
-
-    protected function markAccountNumberFieldAsFailed(GF_Field $field): void
-    {
-        $field->failed_validation = true;
-        $field->validation_message = $this->validationMessage;
     }
 }
--- src/Validators/DirectDebitCapableValidator.php
+++ src/Validators/DirectDebitCapableValidator.php
@@ -4,27 +4,16 @@ declare(strict_types=1);
 
 namespace Itineris\GFLoqateBankVerification\Validators;
 
-use GF_Field;
 use Itineris\GFLoqateBankVerification\API\Result;
 
 class DirectDebitCapableValidator extends AbstractValidator
 {
-    use ValidationMessageConstructTrait;
+    use SetAccountNumberFieldValidationMessageTrait;
+    use SetSortCodeFieldValidationMessageTrait;
+    use ValidationMessageTrait;
 
     protected function shouldIntercept(Result $result): bool
     {
         return $result->isCorrect() && ! $result->isDirectDebitCapable();
-    }
-
-    protected function markSortCodeFieldAsFailed(GF_Field $field): void
-    {
-        $field->failed_validation = true;
-        $field->validation_message = $this->validationMessage;
-    }
-
-    protected function markAccountNumberFieldAsFailed(GF_Field $field): void
-    {
-        $field->failed_validation = true;
-        $field->validation_message = $this->validationMessage;
     }
 }
